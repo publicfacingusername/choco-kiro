80177494a60444be290d49f2fd6aca6d202c0d46
publicfacingusername <github@mail4j.com>
2025-08-01 00:03:10 -0400

    Add all kiro files prior to package build and push

---
+++ get_checksum.ps1
@@ -0,0 +1,2 @@
+$url = 'https://prod.download.desktop.kiro.dev/releases/202507232058-Kiro-win32-x64.exe'
+$f = "$env:TEMP\Kiro.exe"; Invoke-WebRequest $url -OutFile $f; (Get-FileHash $f -Algorithm SHA256).Hash; Remove-Item $f
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://prod.download.desktop.kiro.dev/releases/202507232058-Kiro-win32-x64.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'Kiro*'
+  checksum      = '85C39886ADAFE36BEEB41EDF4EF794473541BD2C200B73D8D0166F5DEDE5CB99'
+  checksumType  = 'sha256'
+  silentArgs    = "/VERYSILENT"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
