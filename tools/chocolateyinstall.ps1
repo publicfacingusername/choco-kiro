63d3df057f6e6d15c96b108bd1773e32b000e4ad
publicfacingusername <github@mail4j.com>
2026-01-09 12:32:14 -0500

    Update Kiro package to 0.8.86

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://prod.download.desktop.kiro.dev/releases/202508020313-Kiro-win32-x64.exe'
+$url        = 'https://prod.download.desktop.kiro.dev/releases/stable/win32-x64/signed/0.8.86/kiro-ide-0.8.86-stable-win32-x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,10 +8,11 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
   softwareName  = 'Kiro*'
-  checksum      = 'C8C5F4A4817F7823BC6B3287C406EB7DA3347922F4139AC477406EB50E6FDE36'
+  checksum      = '0E4B58B270E17B5F6E9E627EEAE121AC5B1207CE6053D39C2BCDF914A575D9DF'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT"
   validExitCodes= @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
+
